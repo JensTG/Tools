080694f27829ada3fa77a9966ef0d0ae027cc444
Jens Graversen <jens@cetg.dk>
2024-02-26 12:58:44 +0100

    Load vector fra fil

--- Include/map.h
+++ Include/map.h
@@ -11,38 +11,11 @@
 using namespace glm;
 using namespace std;
 
-class Model
-{
-    public:
-    VAO shape = VAO();
-    vec3 position = vec3(1.0f, 1.0f, 1.0f);
-    vec3 scale = vec3(1.0f, 1.0f, 1.0f);
-    vec3 rotation = vec3(1.0f, 1.0f, 1.0f);
-    Model(VAO form, vec3 pos, vec3 s, vec3 rot)
-    {
-        shape = form;
-        position = pos;
-        scale = s;
-        rotation = rot;
-    }
-};
-class Collection
-{
-    // Format:
-    // shape, posX, posY, posZ, scaleX, scaleY, scaleZ, degrees, rotX, rotY, rotZ 
-    // cube, 0.0, 2.3, 3.5, 1.0, 1.0, 1.0, 30.0, 1.0, 0.0, 0.0
+// Perhaps use readline to get data
+// Format:
+// shape, posX, posY, posZ, scaleX, scaleY, scaleZ, degrees, rotX, rotY, rotZ 
+// cube, 0.0, 2.3, 3.5, 1.0, 1.0, 1.0, 30.0, 1.0, 0.0, 0.0
 
-    public:
-    vector<Model> models = {};
-
-    /*
-        input: The path to the .col file and optionally scales every shape in the map
-    */
-    Collection(string colPath, vec3 gMove = vec3(1.0f, 1.0f, 1.0f), vec3 gScale = vec3(1.0f, 1.0f, 1.0f), vec3 gRot = vec3(1.0f, 1.0f, 1.0f))
-    {
-        
-    }
-
-};
+// Multiply vertices by translation, scale and rotation. Combine all vertices and indices, and make VAO for a static environment
 
 #endif
---
+++ Include/rendering.h
@@ -0,0 +1,9 @@
+#ifndef RENDERFUNCS
+#define RENDERFUNCS
+#include <VAO.h>
+#include <shader.h>
+#include <map.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#endif
