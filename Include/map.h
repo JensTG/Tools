bf6e392156bd11a52feac91659dbe2351bf546f0
Jens Graversen <jens@cetg.dk>
2024-02-27 12:37:43 +0100

    Loads a collection of shapes as a VAO vector

--- Include/map.h
+++ Include/map.h
@@ -4,7 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
+#include <fstream>
+
 #include <VAO.h>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -13,10 +16,33 @@ using namespace std;
 
 // Perhaps use readline to get data
 // Format:
-// shape, posX, posY, posZ, scaleX, scaleY, scaleZ, degrees, rotX, rotY, rotZ 
-// cube, 0.0, 2.3, 3.5, 1.0, 1.0, 1.0, 30.0, 1.0, 0.0, 0.0
+// C:\VSC_PRO_B\OpenGL\resources\shapes\shape, posX, posY, posZ, scaleX, scaleY, scaleZ, degrees, rotX, rotY, rotZ 
+// C:\VSC_PRO_B\OpenGL\resources\shapes\cube, 0.0, 2.3, 3.5, 1.0, 1.0, 1.0, 30.0, 1.0, 0.0, 0.0
 
 // Multiply vertices by translation, scale and rotation. Combine all vertices and indices, and make VAO for a static environment
 
+vector<VAO> readCollection(string filePath)
+{
+    vector<VAO> shapes = {};
+    ifstream file(filePath.c_str());
+    
+    float posX, posY, posZ, scaleX, scaleY, scaleZ, degrees, rotX, rotY, rotZ;
+    string shapePath;
+    while (file >> shapePath >> posX >> posY >> posZ >> scaleX >> scaleY >> scaleZ >> degrees >> rotX >> rotY >> rotZ)
+    {
+        vector<float> ver = fileToVector<float>(shapePath + ".v");
+        vector<unsigned int> ind = fileToVector<unsigned int>(shapePath + ".i");
+        
+        mat4 trans = mat4(1.0f);
+        trans = translate(trans, vec3(posX, posY, posZ));
+        trans = rotate(trans, radians(degrees), vec3(rotX, rotY, rotZ));
+        trans = scale(trans, vec3(scaleX, scaleY, scaleZ));
+        ver = transformVector(ver, trans);
+
+        shapes.push_back(VAO(ver, ind));
+    }
+
+    return shapes;
+}
 
 #endif
--- Include/rendering.h
+++ Include/rendering.h
@@ -20,36 +20,13 @@ using namespace std;
 template<typename T>
 vector<T> fileToVector(string filePath)
 {
-    ifstream file;
-    string data;
+    ifstream file(filePath);
     vector<T> values;
-    // Ensure ifstreams can throw exceptions:
-    file.exceptions(ifstream::failbit | ifstream::badbit);
 
-    // Process files:
-    try
-    {
-        file.open(filePath.c_str());
-        stringstream stream;
-        stream << file.rdbuf();
-        file.close();
-        data = stream.str();
-    }
-    catch(ifstream::failure e)
-    {
-        cout << "ERROR: Vector data not read: " << e.what() << endl;
-    }
-    
-    size_t index = 0;
-    data.erase(remove_if(data.begin(), data.end(), ::isspace), data.end());
-    while(true)
-    {
-        T value = (T)stof(data, &index);
+    T value;
+    while(file >> value)
         values.push_back(value);
-        if(data.length() > index)
-            data = data.substr(index + 1);
-        else break;
-    }
+
     return values;
 }
 
@@ -58,8 +35,8 @@ vector<float> transformVector(vector<float> vertices, mat4 trans)
     vec4 temp;
     for(int i = 0; i < vertices.size(); i += 3)
     {
-        temp = vec4(vertices[i], vertices[i + 1], vertices[i + 2], 1);
-        temp *= trans;
+        temp = vec4(vertices[i], vertices[i + 1], vertices[i + 2], 1.0f);
+        temp = trans * temp;
         vertices[i] = temp.x;
         vertices[i + 1] = temp.y;
         vertices[i + 2] = temp.z;
