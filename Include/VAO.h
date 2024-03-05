cea3c1994b2faf96990ed908cc2fbdb02c3442b1
Jens Graversen <jens@cetg.dk>
2024-03-05 12:41:55 +0100

    Forstår stadig ikke cubes, måske plader istedet

--- Include/VAO.h
+++ Include/VAO.h
@@ -13,7 +13,7 @@ class VAO
     public:
     unsigned int ID = 0;
     unsigned int nInd = 0;
-    unsigned int texOffset = 0;
+    unsigned int bufferOffset = 0;
 
     VAO();
 
@@ -35,8 +35,10 @@ class VAO
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(), indices.data(), GL_STATIC_DRAW);
         // 4. then set the vertex attributes pointers
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)bufferOffset);
         glEnableVertexAttribArray(0);
+
+        bufferOffset += vertices.size() * sizeof(float);
     }
 
     VAO(string shapePath)
@@ -50,7 +52,6 @@ class VAO
             cout << "ERROR: Incorrect number of indices" << endl;
         
         nInd = indices.size();
-        texOffset = vertices.size() * sizeof(float);
 
         unsigned int VBO, EBO;
         glGenBuffers(1, &VBO);
@@ -66,17 +67,23 @@ class VAO
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(), indices.data(), GL_STATIC_DRAW);
         // 4. then set the vertex attributes pointers
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)bufferOffset);
         glEnableVertexAttribArray(0);
+
+        bufferOffset += vertices.size() * sizeof(float);
     }
 
     void attachTexture(string texPath)
     {
+        glBindVertexArray(ID);
         vector<float> textures = fileToVector<float>(texPath + ".t");
         if(textures.size() % 2 != 0)
             cout << "ERROR: Incorrect number of texture components" << endl;
-        
-
+        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * textures.size(), textures.data(), GL_STATIC_DRAW);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)bufferOffset);
+        glEnableVertexAttribArray(2);
+        glBindVertexArray(0);
+        bufferOffset += textures.size() * sizeof(float);
     }
     
     void bind()
